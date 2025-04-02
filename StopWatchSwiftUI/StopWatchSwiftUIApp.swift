8e65173487a2dd3c1f351504c4b11a114bc1686b
Mohammad <ma@bornak.de>
2025-04-02 22:27:27 +0330

    Initial Commit

---
+++ StopWatchSwiftUI/StopWatchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchSwiftUIApp.swift
+//  StopWatchSwiftUI
+//
+//  Created by Mohammad on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
